9c4b102a5bf93b5ec75c315a6801395e0402bc14
introper <michalhasic03@seznam.cz>
2022-04-21 21:50:05 +0200

    Registrace

--- wp-content/themes/Theme1/template-register.php
+++ wp-content/themes/Theme1/template-register.php
@@ -1,27 +1,72 @@
 <?php
 /*Template name: Registrace */
 get_header();
+
+global $wpdb;
+
+if ($_POST) {
+
+    $email = $wpdb->escape($_POST['email']);
+    $jmeno = $wpdb->escape($_POST['jmeno']);
+    $heslo = $wpdb->escape($_POST['heslo']);
+    $heslo2 = $wpdb->escape($_POST['heslo2']);
+
+    $error = array();
+
+    if(strpos($jmeno, ' ')!==false){
+        $error['jmeno_space'] = "Jméno nesmí obsahovat mezeru";
+    }
+
+    if(empty($jmeno)){
+        $error['jmeno_empty'] = "Jméno je povinné";
+    }
+
+    if(username_exists($jmeno)){
+        $error['jmeno_exist'] = "Jméno již existuje";
+    }
+
+    if(!is_email($email)){
+        $error['email_valid'] = "Email nemá správné hodnoty";
+    }
+
+    if(email_exists($email)){
+        $error['email_exist'] = "Email je již zabrán";
+    }
+
+    if(strcmp($heslo , $heslo2) !==0) {
+        $error['heslo'] = "Hesla se neshodují";
+    }
+
+    if(count($error) ==0){
+        wp_create_user($jmeno, $heslo, $email);
+        echo "Uživatel byl vytvořen";
+        exit();
+    }else{
+        print_r($error);
+    }
+}
+
 ?>
 
 <main>
     <div class="register">
-        <form action="">
+        <form method="POST">
         <h1>Registrovat</h1>
             <div class="register-div">
-                <input type="email" name="email" placeholder="E-mail">
+                <input type="email" name="email" id="email" placeholder="E-mail">
             </div>
 
             <div class="register-div">
-                <input type="text" name="jmeno" placeholder="Uživatelské jméno">
+                <input type="text" name="jmeno" id="jmeno" placeholder="Uživatelské jméno">
             </div>
 
 
             <div class="register-div">
-                <input type="password" name="heslo" placeholder="Heslo">
+                <input type="password" name="heslo" id="heslo" placeholder="Heslo">
             </div>
 
             <div class="register-div">
-                <input type="password" name="zopkujte heslo" placeholder="Zopakujte heslo">
+                <input type="password" name="heslo2" id="heslo2" placeholder="Zopakujte heslo">
             </div>
             <div class="register-div">
                     
