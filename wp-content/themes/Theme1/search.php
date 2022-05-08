f1640903409e763b760e90ce43f5e6fd6b283a16
introper <michalhasic03@seznam.cz>
2022-05-08 10:24:48 +0200

    yes

--- wp-content/themes/Theme1/search.php
+++ wp-content/themes/Theme1/search.php
@@ -15,7 +15,22 @@ $query = new WP_Query($arg);
 if ($query->have_posts()) : ?>
 
     <?php while ($query->have_posts()) : $query->the_post(); ?>
-        <?php echo get_the_title($post->ID); ?>
+        
+        <div class="block-2">
+                            <div class="book-block">
+                                <a href="<?php echo get_the_permalink($post->ID); ?>" class="image-block">
+                                    <img src="<?php echo get_the_post_thumbnail_url($post->ID); ?>" alt="<?php echo get_the_title($post->ID); ?>">
+                                </a> <br>
+                                <div class="text-block">
+                                    <h3><?php echo get_the_title($post->ID); ?></h3>
+                                    <h3><?php echo get_field("cena", $post->ID); ?> Kč</h3>
+                                    <p><?php echo get_the_excerpt($post->ID); ?></p>
+                                    <!-- <div class="btn-block"> -->
+                                        <a href="<?php echo get_the_permalink($post->ID); ?>" class="book-btn">Zobrazit</a>
+                                    <!-- </div> -->
+                                </div>
+                            </div>
+                    </div>
     <?php endwhile; ?>
     <?php wp_reset_query(); ?>
 <?php endif; ?>
